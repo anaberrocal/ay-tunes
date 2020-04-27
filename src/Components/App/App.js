beb3696258e3d8d4952c4c21fa9ef7d97e402dc8
Ana Berrocal <anaberrocalmail@gmail.com>
2020-04-26 22:32:48 -0400

    init repo

---
+++ src/Components/App/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="titleContainer">
+      <h1>
+        Ay!<span class="highlight">Tunes</span>
+      </h1>
+      </div>
+      <div>
+        {/* <SearchBar/> */}
+        <div class="App-playlist">
+          {/* <SearchResults/> */}
+          {/* <Playlist/> */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
