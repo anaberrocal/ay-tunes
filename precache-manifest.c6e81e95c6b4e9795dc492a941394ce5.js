0329119d8a38858d10219bba0489529a41bd8f37
Ana Berrocal <anaberrocalmail@gmail.com>
2020-05-16 16:04:11 -0400

    Updates

---
+++ precache-manifest.c6e81e95c6b4e9795dc492a941394ce5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "444671386a1a698f6a95908ae546f099",
+    "url": "/ay-tunes/index.html"
+  },
+  {
+    "revision": "7151a34bbc122cd7c19d",
+    "url": "/ay-tunes/static/css/main.3e8f509b.chunk.css"
+  },
+  {
+    "revision": "7144ac4211cec7d5d0b8",
+    "url": "/ay-tunes/static/js/2.16e124a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ay-tunes/static/js/2.16e124a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7151a34bbc122cd7c19d",
+    "url": "/ay-tunes/static/js/main.242b9639.chunk.js"
+  },
+  {
+    "revision": "3d9bc1760da4027af12c",
+    "url": "/ay-tunes/static/js/runtime-main.d10df87f.js"
+  },
+  {
+    "revision": "01d221d4418309ef6fcc684931f57571",
+    "url": "/ay-tunes/static/media/Ay-Tunes.01d221d4.png"
+  }
+]);
